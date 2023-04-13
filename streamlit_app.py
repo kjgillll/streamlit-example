8eae3734e553300850ce328399893da72d252068
kjgillll <keronn.gill@my.uwi.edu>
2023-04-13 13:07:58 +0000

    SSL Testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import requests
+import certifi
+import urllib3
 
 from bs4 import BeautifulSoup
 
@@ -11,5 +14,23 @@ from bs4 import BeautifulSoup
 
 BeautifulSoup Scaper Testing
 """
+
+# # print(certifi.where())
+# verify = "/home/gitpod/.pyenv/versions/3.11.1/lib/python3.11/site-packages/certifi/cacert.pem"
+
+URL = "https://devbusiness.un.org/content/site-search"
+r = requests.get(URL, verify="/home/gitpod/.pyenv/versions/3.11.1/lib/python3.11/site-packages/certifi/cacert.pem")
+st.write(r.content)
+
+
+# from urllib3.util.ssl_ import create_urllib3_context
+
+# ctx = create_urllib3_context()
+# ctx.load_default_certs()
+# ctx.options |= 0x4  # ssl.OP_LEGACY_SERVER_CONNECT
+
+# with urllib3.PoolManager(ssl_context=ctx) as http:
+#     r = requests.get("https://devbusiness.un.org/content/site-searchs")
+#     print(r.content)
 
 
