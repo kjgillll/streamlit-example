e3256e661958b6b7754448b18ae20d3f31840a26
kjgillll <keronn.gill@my.uwi.edu>
2023-04-13 17:47:44 +0000

    Json Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,9 @@ soup = BeautifulSoup(r.content, 'html5lib')
 st.write(soup.prettify())
 
 json = requests.get("https://devbusiness.un.org/solr-sitesearch-output/10//0/ds_field_last_updated/desc?bundle_fq=procurement_notice&sm_vid_Institutions_fq=&sm_vid_Procurement_Type_fq=&sm_vid_Countries_fq=&sm_vid_Sectors_fq=&sm_vid_Languages_fq=&sm_vid_Notice_Type_fq=&deadline_multifield_fq=&ts_field_project_name_fq=&label_fq=&sm_field_db_ref_no__fq=&sm_field_loan_no__fq=&dm_field_deadlineFrom_fq=&dm_field_deadlineTo_fq=&ds_field_future_posting_dateFrom_fq=&ds_field_future_posting_dateTo_fq=&bm_field_individual_consulting_fq=").json()
-st.write(json)
-
+# st.write(json)
+df = pd.DataFrame.from_dict(json)
+st.write(df)
 # from urllib3.util.ssl_ import create_urllib3_context
 
 # ctx = create_urllib3_context()
