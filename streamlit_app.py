2470ca9041558db01a1b555eb3f922cef549fe87
kjgillll <keronn.gill@my.uwi.edu>
2023-04-13 14:47:34 +0000

    Add header to request

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,8 @@ BeautifulSoup Scaper Testing
 # verify = "/home/gitpod/.pyenv/versions/3.11.1/lib/python3.11/site-packages/certifi/cacert.pem"
 
 URL = "https://devbusiness.un.org/content/site-search"
-r = requests.get(URL)
+headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
+r = requests.get(URL, headers=headers)
 soup = BeautifulSoup(r.content, 'html5lib')
 st.write(soup.prettify())
 
